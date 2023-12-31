395d5c4b74395cc925c34da5c195f1b0c691c240
Mani <140153681+MXNIHACKER@users.noreply.github.com>
2024-01-01 03:02:22 +0530

    Update bot.py

--- AltBots/modules/bot.py
+++ AltBots/modules/bot.py
@@ -21,10 +21,10 @@ from datetime import datetime
 async def ping(e):
     if e.sender_id in SUDO_USERS:
         start = datetime.now()
-        altron = await e.reply(f"» __ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ ᴛᴏ ғᴜᴄᴋ ᴛʜᴇsᴇ ʙɪᴛᴄʜᴇs__\n☞𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 : [𝐓ᴇᴀᴍ 𝐇ᴜɴᴛᴇʀ](https://t.me/Team_Hunter_X)")
+        altron = await e.reply(f"» __ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ ᴛᴏ ғᴜᴄᴋ ᴛʜᴇsᴇ ʙɪᴛᴄʜᴇs__")
         end = datetime.now()
         mp = (end - start).microseconds / 1000
-        await altron.edit(f"__⚡ ᴘɪɴɢ__\n» `{mp} ᴍꜱ`")
+        await altron.edit(f"__⚡ ᴘɪɴɢ__\n» `{mp} ᴍꜱ`\n\n☞𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 : [𝐓ᴇᴀᴍ 𝐇ᴜɴᴛᴇʀ](https://t.me/Team_Hunter_X)")
     
 
 
