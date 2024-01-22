09e1035c0ea3f7bb591250f32f8643a7d658fbbb
BAD BABY <154504921+Badhacker98@users.noreply.github.com>
2024-01-23 01:51:42 +0530

    Update spam.py

--- AltBots/modules/spam.py
+++ AltBots/modules/spam.py
@@ -2,7 +2,7 @@
 import asyncio
 
 from AltBots.data import GROUP, PORMS
-from config import X1, X2, X3, X4, X5 , X6, X7, X8, X9, X10, SUDO_USERS, CMD_HNDLR as hl
+from config import X1, X2, X3, X4, X5 , X6, X7, X8, X9, X10, X11, X12 ,X13, SUDO_USERS, CMD_HNDLR as hl
 
 from random import choice
 from telethon import events, functions, types
